4dec4d8aaca1ad26be961e70cd4d02791c6009d0
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-13 23:17:18 +0300

    Stack navigator added.

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen component={Home} name="home" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
