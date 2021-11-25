f7627865a13a42e1f080aacc08d40f9adc61b023
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-26 01:48:40 +0300

    dummy screens for bottom navigation.

---
+++ screens/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#E5E5E5" }}>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
---
+++ screens/Screen1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Screen1 = () => {
+  return (
+    <SafeAreaView style={{flex : 1, backgroundColor: "#E5E5E5"}}>
+      <Text>Screen1</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Screen1
+
+const styles = StyleSheet.create({})
