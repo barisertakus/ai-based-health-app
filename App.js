5218a17b53fcfd96e0e8614cb1d12b02867ac599
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-05 18:12:26 +0300

    add logbox to app

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import RootNavigation from './RootNavigation';
+import React from "react";
+import { StyleSheet, Text, View, LogBox } from "react-native";
+import RootNavigation from "./RootNavigation";
+
+LogBox.ignoreLogs([
+  "Constants.platform.ios.model has been deprecated in favor of expo-device's Device.modelName property. This API will be removed in SDK 45."])
 
 export default function App() {
-  return (
-    <RootNavigation />
-  );
+  return <RootNavigation />;
 }
